29958be979098310ff56be4a5cde532c768eda65
clairedotcom <claireacarson@gmail.com>
2021-10-01 12:37:25 -0400

    Add test for #add_move in game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,5 @@
+require_relative '../lib/game.rb'
+
 describe Game do
     #initialize game with two players and a board
     
@@ -17,8 +19,22 @@ describe Game do
     end    
 
     #if it's a valid move add to board
-    #board.update_board
-    
+    # send signal to board to update
+    describe '#add_move' do
+        context 'when the a move has been selected' do
+            let(:board_update) { instance_double(Board)}
+            let(:game_update) { described_class.new}
+
+            before do
+                allow(game_update).to receive(:get_move).with(3)
+            end
+            
+            it 'sends value to board class' do
+                expect(board_update).to receive(:update_board).with(3)
+                game_update.add_move
+            end    
+        end
+    end    
     
     #switch players
     describe '#switch_players' do
